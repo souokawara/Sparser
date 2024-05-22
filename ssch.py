43c6b1da963edb17e53cc91b62dedc86656c1778
So Okawara <ba.sokawara2023@gmail.com>
2024-05-23 05:40:10 +0900

    first commit

---
+++ ssch.py
@@ -0,0 +1,52 @@
+"""
+Sparse Matrix & Rad Matrix Schemer
+
+For the use of the LA processes in ML or Quantum Annealing emulation.
+
+
+#############
+Sparse Matrix
+#############
+
+The universal circuit schemer in matrix.
+
+    Attributes
+
+    Functions
+
+
+
+##########
+Rad Matrix
+##########
+
+The specified Sparse Matrix for complex analysis.
+
+    Attributes
+
+    Functions
+
+
+"""
+# This code conforms to the PEP 8 style guide.
+
+
+# import os
+# import sys
+
+import pbd
+import numpy as np
+import pandas as pd
+
+
+#Sparse Matrix class
+class SparseMatrix:
+
+    def __init__(self):
+        self.n = 0
+
+#Rad Matrix class
+class RadMatrix:
+
+    def __init__(self):
+        self.n = 0
